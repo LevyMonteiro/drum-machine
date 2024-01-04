12277c0746d866b7e499dd141f55b9982662f0dd
Levy Monteiro <levymonteiro268@gmail.com>
2024-01-04 17:02:29 -0300

    create Keyboard component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,81 +1,12 @@
-import { useEffect, useContext } from 'react';
 import '../App.css';
-import { ControllerContext } from '../context/ControllerContext';
 import Controller from './Controller';
+import Keyboard from './Keyboard';
 
 function App() {
-  const { setDisplayAss, soundGroup, volume, power } =
-    useContext(ControllerContext);
-
-  // track the pressed key
-  useEffect(() => {
-    document.addEventListener('keydown', (event) => {
-      playSound(event.key.toUpperCase());
-    });
-  });
-
-  const playSound = (selector) => {
-    const audio = document.getElementById(selector);
-    audio.currentTime = 0;
-    audio.play();
-
-    // show the audio description on diplay
-    setDisplayAss(power ? audio.textContent : '');
-
-    // add animation when the drum-pad is pressed
-    const drumPadElement = audio.parentNode;
-
-    drumPadElement.classList.add('pressed');
-    setTimeout(function () {
-      drumPadElement.classList.remove('pressed');
-    }, 100);
-  };
-
-  const setAudioVolume = () => {
-    const audios = soundGroup.map((sound) =>
-      document.getElementById(sound.text),
-    );
-    audios.forEach((audio) => {
-      if (audio) {
-        audio.volume = volume;
-      }
-    });
-  };
-
-  setAudioVolume();
-
   return (
     <div id='drum-machine'>
       <Controller />
-      <div className='drum-pads'>
-        {power
-          ? soundGroup.map((drumPad) => (
-              <div
-                className='drum-pad'
-                id={drumPad['audio-id']}
-                key={drumPad.src}
-                onClick={() => playSound(drumPad.text)}
-              >
-                {drumPad.text}
-                <audio id={drumPad.text} className='clip' src={drumPad.src}>
-                  {drumPad['audio-id']}
-                </audio>
-              </div>
-            ))
-          : soundGroup.map((drumPad) => (
-              <div
-                className='drum-pad'
-                id={drumPad['audio-id']}
-                key={drumPad.src}
-                onClick={() => playSound(drumPad.text)}
-              >
-                {drumPad.text}
-                <audio id={drumPad.text} className='clip' src={'#'}>
-                  {drumPad['audio-id']}
-                </audio>
-              </div>
-            ))}
-      </div>
+      <Keyboard />
       <footer>
         by{' '}
         <a href='https://github.com/levymonteiro' target='_blank'>
---
+++ src/components/Keyboard.jsx
@@ -0,0 +1,62 @@
+import { useContext, useEffect } from 'react';
+import { ControllerContext } from '../context/ControllerContext';
+
+export default function Keyboard() {
+  const { power, setDisplayAss, soundGroup } = useContext(ControllerContext);
+
+  const playSound = (selector) => {
+    const audio = document.getElementById(selector);
+    audio.currentTime = 0;
+    audio.play();
+
+    // show the audio description on display
+    setDisplayAss(power ? audio.textContent : '');
+
+    // add animation when the drum-pad is pressed
+    const drumPadElement = audio.parentNode;
+
+    drumPadElement.classList.add('pressed');
+    setTimeout(function () {
+      drumPadElement.classList.remove('pressed');
+    }, 100);
+  };
+
+  // track the pressed key
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      playSound(event.key.toUpperCase());
+    });
+  });
+
+  return (
+    <div className='drum-pads'>
+      {power
+        ? soundGroup.map((drumPad) => (
+            <div
+              className='drum-pad'
+              id={drumPad['audio-id']}
+              key={drumPad.src}
+              onClick={() => playSound(drumPad.text)}
+            >
+              {drumPad.text}
+              <audio id={drumPad.text} className='clip' src={drumPad.src}>
+                {drumPad['audio-id']}
+              </audio>
+            </div>
+          ))
+        : soundGroup.map((drumPad) => (
+            <div
+              className='drum-pad'
+              id={drumPad['audio-id']}
+              key={drumPad.src}
+              onClick={() => playSound(drumPad.text)}
+            >
+              {drumPad.text}
+              <audio id={drumPad.text} className='clip' src={'#'}>
+                {drumPad['audio-id']}
+              </audio>
+            </div>
+          ))}
+    </div>
+  );
+}

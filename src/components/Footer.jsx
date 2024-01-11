317fa494c5ca87e782b07754d9ff1fa6e7bcb9cd
Levy Monteiro <levymonteiro268@gmail.com>
2024-01-11 14:35:04 -0300

    refactoring footer css with tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 export default function Footer() {
   return (
-    <footer className='text-white m-8'>
-      by{' '}
-      <a href='https://github.com/levymonteiro' target='_blank'>
+    <footer className='text-white m-8 text-shadow text-shadow-black text-shadow-x-3 text-shadow-y-3 text-shadow-blur-3 scale-90'>
+      {'coded by '}
+      <a
+        className='font-medium'
+        href='https://github.com/levymonteiro'
+        target='_blank'
+      >
         Levy Monteiro
       </a>
     </footer>
